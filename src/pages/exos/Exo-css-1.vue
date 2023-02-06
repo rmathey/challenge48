679bec21a09b9e233b4037c615c6c39e54b93622
Matéo PALMIERI <120114578+MateoPalmieri@users.noreply.github.com>
2023-02-06 14:47:50 +0100

    ajouts pages exercices css et sécurité

---
+++ src/pages/exos/Exo-css-1.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1><a href="/">Retour</a></h1>
+    <h1>Exercice CSS 1 : niveau facile</h1>
+</template>
+
+<script>
+export default {
+    components: {
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,30 @@ const routes = [
   {
     path: "/exotest",
     component: () => import("../pages/exos/ExoTest.vue")
+  },
+  {
+    path: "/exercice-css-1",
+    component: () => import("../pages/exos/Exo-css-1.vue")
+  },
+  {
+    path: "/exercice-css-2",
+    component: () => import("../pages/exos/Exo-css-2.vue")
+  },
+  {
+    path: "/exercice-css-3",
+    component: () => import("../pages/exos/Exo-css-3.vue")
+  },
+  {
+    path: "/exercice-securite-1",
+    component: () => import("../pages/exos/Exo-securite-1.vue")
+  },
+  {
+    path: "/exercice-securite-2",
+    component: () => import("../pages/exos/Exo-securite-2.vue")
+  },
+  {
+    path: "/exercice-securite-3",
+    component: () => import("../pages/exos/Exo-securite-3.vue")
   }
 ]
 
