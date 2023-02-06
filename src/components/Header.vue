4fd821867244256b745fde1cfa741749a35a2ffb
0ri0n09 <jonathan.jaremczuk@ynov.com>
2023-02-06 16:06:45 +0100

    test1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,46 +3,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tailwind Starter Template - Ghostwind CSS : Tailwind Toolbox</title>
-    <meta name="author" content="name">
+    <title>Challenge 48H - Groupe 3</title>
+    <meta name="Groupe 3" content="name">
     <meta name="description" content="description here">
     <meta name="keywords" content="keywords,here">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/> <!--Replace with your tailwind.css once created-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" rel="stylesheet">
   </head>
 
-  <body class="bg-gray-200 font-sans leading-normal tracking-normal">
-  <!--Header-->
-  <div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image:url('https://wallpaperaccess.com/full/1803545.jpg'); height: 60vh; max-height:460px;">
-    <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
-      <!--Title-->
-      <p class="text-white font-extrabold text-3xl md:text-5xl"></p>
-      <p class="text-xl md:text-2xl text-gray-200 font-bold">Groupe 3 - Challenge 48H</p>
+  <div class="bg-gray-100 font-sans leading-normal tracking-normal">
+    <div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image:url('https://wallpaperaccess.com/full/1803545.jpg'); height: 50vh; max-height:200px;">
+      <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+        <p class="text-white font-extrabold text-3xl md:text-5xl"></p>
+        <p class="text-xl md:text-2xl text-gray-200 font-bold">Groupe 3 - Challenge 48H</p>
+      </div>
     </div>
   </div>
-
-  <!--Container-->
-  <div class="container px-4 md:px-0 max-w-2xl mx-auto -mt-32">
-    <div class="mx-0 sm:mx-6">
-      <!--Nav-->
-      <nav class="mt-0 w-full">
-        <div class="container mx-auto flex items-center">
-            <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
-              <li class="mr-2">
-                <a class="inline-block py-2 px-2 text-white no-underline hover:underline" href="post.html">POST</a>
-              </li>
-              <li class="mr-2">
-                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:underline py-2 px-2" href="#">LINK</a>
-              </li>
-              <li class="mr-2">
-                <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:underline py-2 px-2" href="#">LINK</a>
-              </li>
-            </ul>
-        </div>
-      </nav>
-    </div>
-  </div>
-  </body>
 </template>
 <script>
 export default {
