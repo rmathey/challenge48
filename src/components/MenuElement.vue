53a413ddfd43da1a41edb3da0645a55e78cf6ddd
rmathey <rmathey810@gmail.com>
2023-02-07 10:24:16 +0100

    checkbox

--- src/components/MenuElement.vue
+++ src/components/MenuElement.vue
@@ -1,11 +1,40 @@
 <template>
     <li>
-        <h2><a :href="element.url">{{ element.titre }}</a> ⬛ ou ☑️</h2>
+        <h2 class="container"><a :href="element.url">{{ element.titre }}</a>
+            <h6 v-if="!valide">⬛</h6>
+            <h6 v-else>☑️</h6>
+        </h2>
     </li>
 </template>
 
 <script>
 export default {
-    props: ['element']
+    props: ['element'],
+    data() {
+        return {
+            valide: false
+        }
+    },
+    mounted() {
+        if (localStorage.getItem(this.element.id)) {
+            this.valide = true;
+            console.log(this.valide)
+        }
+    }
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+h6 {
+  width: 50px;
+  text-align: center;
+}
+
+
+</style>
